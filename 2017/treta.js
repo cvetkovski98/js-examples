90652cf5dea9d6c7df31d76285ba1308523022a9
Oliver Cvetkovski <cvetkovski98@gmail.com>
2020-11-25 20:58:22 +0100

    feat: refactor NPC task

--- 2017/treta.js
+++ 2017/treta.js
@@ -1,43 +1,42 @@
-class NPC {
-    static NPCs = 0;
-
-    constructor(name, hitpoints, is_hero = false) {
-        this.name = name ? name : ++NPC.NPCs;
-        this.hitpoints = hitpoints ? hitpoints : Math.floor(Math.random() * 100 + 1);
-        this.is_hero = is_hero
-        this.is_alive = true
-    }
-
-    status() {
-        return `<Status name=${this.name}, hp=${this.hitpoints}/>`;
-    }
+function NPC(name, hitpoints, is_hero = false) {
+    this.name = `NPC ${NPC.NPCs++}`
+    this.hitpoints = hitpoints
+    this.is_hero = is_hero
+    this.is_alive = true
 }
 
-class Hero extends NPC {
-    constructor(name, hitpoints = 100, damage = 10, has_critical = false) {
-        super(name, hitpoints, true);
-        this.damage = damage;
-        this.has_critical = has_critical
-    }
+NPC.NPCs = 0
 
-    attack(victim) {
-        let amplifier = this.has_critical ? 1.5 : 1.0
-        let total_damage = amplifier * this.damage
-        if (this.has_critical)
-            console.log(`<CriticalAttack from=${this.name} to=${victim.name} damage=${total_damage}/>`);
-        else
-            console.log(`<Attack from=${this.name} to=${victim.name} damage=${total_damage}/>`);
-        victim.hitpoints -= total_damage
-        if (victim.hitpoints <= 0)
-            victim.is_alive = false
-        if (victim.is_hero && this.has_critical) {
-            if (victim.is_alive) {
-                victim.has_critical = true
-            }
-            this.has_critical = false
+NPC.prototype.status = function () {
+    console.log(`<Status name=${this.name}, hp=${this.hitpoints}/>`);
+}
+
+function Hero(name, hitpoints = 100, damage = 10, has_critical = false) {
+    NPC.call(this, name, hitpoints, true)
+    this.damage = damage
+    this.has_critical = has_critical
+}
+
+Hero.prototype = Object.create(NPC.prototype)
+Hero.prototype.constructor = Hero
+
+Hero.prototype.attack = function (victim) {
+    let amplifier = this.has_critical ? 1.5 : 1.0
+    let total_damage = amplifier * this.damage
+    if (this.has_critical)
+        console.log(`<CriticalAttack from=${this.name} to=${victim.name} damage=${total_damage}/>`);
+    else
+        console.log(`<Attack from=${this.name} to=${victim.name} damage=${total_damage}/>`);
+    victim.hitpoints -= total_damage
+    if (victim.hitpoints <= 0)
+        victim.is_alive = false
+    if (victim.is_hero && this.has_critical) {
+        if (victim.is_alive) {
+            victim.has_critical = true
         }
-        console.log(this.status())
+        this.has_critical = false
     }
+    this.status()
 }
 
 const N = 35;
@@ -62,7 +61,7 @@ for (let i = 0; i < N; i++) {
 }
 
 console.log(`NPCs: ${NPC.NPCs}, HEROs: ${heroes_left.length}`)
-heroes_left.filter(it => it.has_critical).forEach(it => console.log(it.status()))
+heroes_left.filter(it => it.has_critical).forEach(it => it.status())
 
 let round = 1
 while (true) {
